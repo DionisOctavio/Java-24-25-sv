debf2896734f6f8c49cb0aff0aec61c778574624
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-14 23:17:55 +0200

    [14/10/2024] 23:17
    [14/10/2024] 23:17

--- 003 Video 2 - Simpson/src/simpsonprogfuncional/SimpsonProgFuncional.java
+++ 003 Video 2 - Simpson/src/simpsonprogfuncional/SimpsonProgFuncional.java
@@ -1,100 +1,95 @@
-   package simpsonprogfuncional;
+package simpsonprogfuncional;
 
-   import java.util.Random;
+import java.util.Random;
 
-   public class SimpsonProgFuncional {
+public class SimpsonProgFuncional {
 
-        private static final int MAX_FILA_TABLERO = 10;
-        private static final int MAX_COLUMNA_TABLERO = 10;
-        private static char [][] tablero;
+    private static final int MAX_FILA_TABLERO = 10;
+    private static final int MAX_COLUMNA_TABLERO = 10;
+    private static char [][] tablero;
 
-        public static void main(String[] args) {
-            //1º inicio matriz tablero
-            tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
-
-            //2º rellenar tablero
-            for (int i = 0; i <MAX_FILA_TABLERO; i++) {
-                for (int j = 0; j <MAX_COLUMNA_TABLERO; j++) {
-                    tablero[i][j] = 'L';
-                }
+    private static void imprimirTablero() {
+        for (int i = 0; i <MAX_FILA_TABLERO; i++) {
+            for (int j = 0; j <MAX_COLUMNA_TABLERO; j++) {
+                System.out.print(tablero[i][j] + " ");
             }
-            //3º imprimir tablero
-            imprimirTablero();
-
             System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-
-            //4º asignar bart
-            Random aleatorio = new Random();
-            int filaleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
-            int columnaaleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-
-            tablero[filaleatorio][columnaaleatorio] = 'B';
-
-            //5º imprimir tablero
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-
-            imprimirTablero();
-
-
-
-            //6º repartir 10 homers dentro del tablero
-            int filaleatorioHomer;
-            int columnaaleatorioHomer;
-            for (int i = 0; i < 10; i++) {
-                do {
-                    filaleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
-                    columnaaleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-
-                }while (tablero[filaleatorioHomer][columnaaleatorioHomer]!='L');
-
-                tablero[filaleatorioHomer][columnaaleatorioHomer] = 'H';
-            }
-
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-
-            imprimirTablero();
-
-            //8º repartir 10 homers dentro del tablero
-            int filaleatorioMuro;
-            int columnaaleatorioMuro;
-            for (int i = 0; i < 10; i++) {
-                do {
-                    filaleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO);
-                    columnaaleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-
-                }while (tablero[filaleatorioMuro][columnaaleatorioMuro]!='L');
-
-                tablero[filaleatorioMuro][columnaaleatorioMuro] = 'M';
-            }
-
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-
-            imprimirTablero();
-
-            tablero[MAX_FILA_TABLERO-1][MAX_COLUMNA_TABLERO-1] = 'F';
-
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-
-            imprimirTablero();
         }
+    }
 
-        // 0º Imprimir Tablero
-        private static void imprimirTablero() {
-            for (int i = 0; i <MAX_FILA_TABLERO; i++) {
-                for (int j = 0; j <MAX_COLUMNA_TABLERO; j++) {
-                    System.out.print(tablero[i][j] + " ");
-                }
-                System.out.println(" ");
+    private static void asignarCaracterACasillas (char caracter) {
+        //2º rellenar tablero
+        for (int i = 0; i <MAX_FILA_TABLERO; i++) {
+            for (int j = 0; j <MAX_COLUMNA_TABLERO; j++) {
+                tablero[i][j] = caracter;
             }
         }
     }
+
+    private static void asignarBartACasillasLibre (char caracter) {
+        Random aleatorio = new Random();
+        int filaleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
+        int columnaaleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+
+        tablero[filaleatorio][columnaaleatorio] = caracter;
+    }
+
+    private static void asignarCaracterACasillasLibres (char caracter) {
+        Random aleatorio = new Random();
+        int filaleatorioHomer;
+        int columnaaleatorioHomer;
+        for (int i = 0; i < 10; i++) {
+            do {
+                filaleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
+                columnaaleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+
+            }while (tablero[filaleatorioHomer][columnaaleatorioHomer]!='L');
+
+            tablero[filaleatorioHomer][columnaaleatorioHomer] = caracter;
+        }
+    }
+
+    public static void main(String[] args) {
+        //1º inicio matriz tablero
+        tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
+
+        //2º Asigno un caracter a la matriz
+        asignarCaracterACasillas('L');
+
+        System.out.println();
+
+        imprimirTablero();
+
+        //3º Asigno a bart
+        asignarBartACasillasLibre('B');
+
+        System.out.println();
+
+        imprimirTablero();
+
+
+        //4º Asigno a 10 Homers
+        asignarCaracterACasillasLibres('H');
+
+        System.out.println();
+
+        imprimirTablero();
+
+
+        //5º Creo 10 Muros
+        asignarCaracterACasillasLibres('M');
+
+        System.out.println();
+
+        imprimirTablero();
+
+        // Final
+        tablero[MAX_FILA_TABLERO-1][MAX_COLUMNA_TABLERO-1] = 'F';
+
+        System.out.println();
+
+        imprimirTablero();
+    }
+
+
+}

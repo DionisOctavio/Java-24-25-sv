abcf092670f02f3c0a77d7bdf113fd7a71e3831d
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-23 13:33:04 +0200

    [23/10/2024] 13:32
    [23/10/2024] 13:32

--- 009 PRIMITIVA/src/Main.java
+++ 009 PRIMITIVA/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Random ale = new Random();
         Scanner leer = new Scanner(System.in);
-
+        int intento = 6;
 
         String arrayPrimitiva[] = new String[6];
         for (int i = 0; i < arrayPrimitiva.length; i++) {
@@ -18,16 +18,25 @@ public class Main {
             System.out.println(arrayPrimitiva[i]);
         }
 
-        System.out.println("Dame un Numerin");
-        String usuario = leer.next();
-        for (int i = 0; i < arrayPrimitiva.length; i++) {
-            if (usuario.equals(arrayPrimitiva[i])) {
-                arrayPrimitiva[i] = "XX";
+        do {
+            System.out.println("Intentos: " + intento);
+            System.out.println("Dame un Numerin");
+            String usuario = leer.next();
+            for (int i = 0; i < arrayPrimitiva.length; i++) {
+                if (usuario.equals(arrayPrimitiva[i])) {
+                    arrayPrimitiva[i] = "XX";
+                }
             }
-        }
 
-        for (int i = 0; i < arrayPrimitiva.length; i++) {
-            System.out.println(arrayPrimitiva[i]);
-        }
+            for (int i = 0; i < arrayPrimitiva.length; i++) {
+                System.out.println(arrayPrimitiva[i]);
+            }
+
+            intento = intento - 1;
+        }while(intento > 0);
+
+        System.out.println("+-----------------+");
+        System.out.println("   End Primitiva  ");
+        System.out.println("+---------------- +");
     }
 }

ee848d1120f4ce2e5ae1f36b0187a0d92ffb9fd8
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-23 22:32:31 +0200

    [23/10/2024] 22:32
    [23/10/2024] 22:32

--- 009 PRIMITIVA/src/Main.java
+++ 009 PRIMITIVA/src/Main.java
@@ -3,51 +3,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static char[][] tablero;
-    private static final int FILA = 5;
-    private static final int COLUMNA = 5;
-    private static Random aleatorio = new Random();
-    private static void rellenarTablero(char personaje){
-        for (int i = 0; i < FILA; i++){
-            for(int j = 0; j > COLUMNA; j++){
-                tablero[i][j] = personaje;
-            }
-        }
-    }
-
-    private static void asignarBart(char caracter){
-        int filaBart = aleatorio.nextInt(FILA);
-        int columnaBart = aleatorio.nextInt(COLUMNA);
-
-        tablero[filaBart][columnaBart] = caracter;
-    }
-
-    private static void asignarPersonajeALibre(char personaje, int numRepeticiones){
-        int filaPersonaje = 0;
-        int columnaPersonaje = 0;
-        for(int i = 0; i < numRepeticiones; i++){
-            do{
-                filaPersonaje = aleatorio.nextInt(FILA);
-                columnaPersonaje = aleatorio.nextInt(COLUMNA);
-            }while (tablero[filaPersonaje][columnaPersonaje] !='L');
-            tablero[filaPersonaje][columnaPersonaje] = personaje;
-        }
-
-    }
-
     public static void main(String[] args) {
-        tablero = new char[FILA][COLUMNA];
+        Random ale = new Random();
+        Scanner leer = new Scanner(System.in);
+        int intento = 6;
 
-        rellenarTablero('L');
+        String arrayPrimitiva[] = new String[6];
+        for (int i = 0; i < arrayPrimitiva.length; i++) {
+            int numAle = ale.nextInt(100);
+            arrayPrimitiva[i] = String.valueOf(numAle);
+        }
 
-        asignarBart('B');
+        for (int i = 0; i < arrayPrimitiva.length; i++) {
+            System.out.println(arrayPrimitiva[i]);
+        }
 
-        asignarPersonajeALibre('H', 10);
+        do {
+            System.out.println("Intentos: " + intento);
+            System.out.println("Dame un Numerin");
+            String usuario = leer.next();
+            for (int i = 0; i < arrayPrimitiva.length; i++) {
+                if (usuario.equals(arrayPrimitiva[i])) {
+                    arrayPrimitiva[i] = "XX";
+                }
+            }
 
-        asignarPersonajeALibre('M', 10);
+            for (int i = 0; i < arrayPrimitiva.length; i++) {
+                System.out.println(arrayPrimitiva[i]);
+            }
 
+            intento = intento - 1;
+        }while(intento > 0);
 
-
-
+        System.out.println("+-----------------+");
+        System.out.println("   End Primitiva  ");
+        System.out.println("+---------------- +");
     }
 }
